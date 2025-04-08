fc6e4d18861f8b932a26aca6292348411f5b0033
cocoba88 <indramario5@gmail.com>
2025-04-08 15:28:14 +0700

    Update StickerPackDetailsActivity.java

--- app/src/main/java/com/ae/apps/stickerapp/StickerPackDetailsActivity.java
+++ app/src/main/java/com/ae/apps/stickerapp/StickerPackDetailsActivity.java
@@ -30,9 +30,6 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ae.apps.stickerapp.reviews.AppReview;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
 
 import java.lang.ref.WeakReference;
 
@@ -99,23 +96,12 @@ public class StickerPackDetailsActivity extends BaseActivity {
             getSupportActionBar().setTitle(showUpButton ? R.string.title_activity_sticker_pack_details_multiple_pack : R.string.title_activity_sticker_pack_details_single_pack);
         }
 
-        initAd();
-
         AppReview.getInstance().launchReviewFlow(this);
     }
 
     private void setToolBar() {
         Toolbar toolbar = findViewById(R.id.app_toolbar);
         setSupportActionBar(toolbar);
-    }
-
-    private void initAd() {
-        MobileAds.initialize(this, initializationStatus -> {});
-        AdView mAdView = findViewById(R.id.adView);
-        // AdResources adResources = new AdResources();
-
-        // https://developers.google.com/admob/android/banner
-        mAdView.loadAd(new AdRequest.Builder().build());
     }
 
     private void launchInfoActivity(String publisherWebsite, String publisherEmail, String privacyPolicyWebsite, String trayIconUriString) {
